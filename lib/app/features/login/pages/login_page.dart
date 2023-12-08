464a70c6729eea2ff1e9a2ea9585c0c9543cc311
eduardasf <147343929+eduardasf@users.noreply.github.com>
2023-12-07 22:17:17 -0300

    Finalização da tela de login

--- lib/app/features/login/pages/login_page.dart
+++ lib/app/features/login/pages/login_page.dart
@@ -31,15 +31,17 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
             Container(
-              padding: const EdgeInsets.all(33.0),
+              padding: const EdgeInsets.only(top: 20),
               child: const Column(
                 children: [
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Text(
                         'Entre com o e-mail acadêmico',
                         textDirection: TextDirection.ltr,
-                        style: TextStyle(fontSize: 20.0, color: Color(0xFF525252)),
+                        style:
+                            TextStyle(fontSize: 20.0, color: Color(0xFF525252)),
                       ),
                     ],
                   ),
@@ -49,7 +51,7 @@ class _LoginPageState extends State<LoginPage> {
             Container(
               width: 330,
               height: 52,
-              margin: const EdgeInsets.only(top: 1),
+              margin: const EdgeInsets.only(top: 20),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(16),
                 color: Colors.blue.withOpacity(0.05),
@@ -95,7 +97,7 @@ class _LoginPageState extends State<LoginPage> {
                   SizedBox(width: 5),
                   Expanded(
                     child: TextField(
-                      obscureText: true, 
+                      obscureText: true,
                       decoration: InputDecoration(
                         hintText: 'Senha',
                         border: InputBorder.none,
@@ -105,6 +107,193 @@ class _LoginPageState extends State<LoginPage> {
                 ],
               ),
             ),
+            Container(
+              margin: const EdgeInsets.only(top: 20),
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 35),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            color: Color(0xFFF0EDFF),
+                            thickness: 2,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 3),
+                          child: Text(
+                            'Entre',
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 3),
+                          child: Text(
+                            'com outras',
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Color.fromARGB(255, 76, 76, 76)),
+                          ),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            color: Color(0xFFF0EDFF),
+                            thickness: 2,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 34, top: 25, right: 34),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  foregroundColor:
+                      const Color.fromARGB(255, 166, 204, 235).withOpacity(0.1),
+                  backgroundColor: Colors.white,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  side: const BorderSide(
+                    width: 1.5,
+                    color: Color(0xFFF0EDFF),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FittedBox(
+                      fit: BoxFit.contain,
+                      child: Image.asset(
+                        'assets/icons/favicon.ico',
+                        width: 24,
+                        height: 24,
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    const Text.rich(
+                      TextSpan(
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                        children: [
+                          TextSpan(text: 'Entre com o '),
+                          TextSpan(
+                            text: 'Google',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 34, top: 25, right: 34),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  foregroundColor:
+                      const Color.fromARGB(255, 166, 204, 235).withOpacity(0.1),
+                  backgroundColor: Colors.white,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 9),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  side: const BorderSide(
+                    width: 1.5,
+                    color: Color(0xFFF0EDFF),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FittedBox(
+                      fit: BoxFit.contain,
+                      child: Image.asset(
+                        'assets/icons/iconFacebook.png',
+                        width: 40,
+                        height: 40,
+                      ),
+                    ),
+                    const SizedBox(width: 0),
+                    const Text.rich(
+                      TextSpan(
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                        children: [
+                          TextSpan(text: 'Entre com o '),
+                          TextSpan(
+                            text: 'Facebook',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0, top: 30),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: const Color(0xFF3A5CC0),
+                  elevation: 4,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 65, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.77),
+                  ),
+                ),
+                child: const Text('Entrar'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 25),
+              child: GestureDetector(
+                onTap: () {
+                },
+                child: const Text.rich(
+                  TextSpan(
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Color(0xFF525252),
+                    ),
+                    children: [
+                      TextSpan(text: 'Não tem conta? '),
+                      TextSpan(
+                        text: 'Cadastre-se aqui',
+                        style: TextStyle(
+                          color: Color(0xFF3A5CC0),
+                          decoration: TextDecoration.underline,
+                          decorationColor: Color(0xFF3A5CC0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
